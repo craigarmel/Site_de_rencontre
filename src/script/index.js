0058e748677bf1428eb0e755fb2d9d244c98c0ac
craigarmel <132368808+craigarmel@users.noreply.github.com>
2024-10-12 20:05:31 +0200

    Création de compte

--- src/script/index.js
+++ src/script/index.js
@@ -1,34 +1,43 @@
-import { initializeApp } from "firebase/app";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAI3z9mlINBKqd8CIF18LV-y0FJKrV7jms",
-    authDomain: "site-de-rencontre-b4121.firebaseapp.com",
-    projectId: "site-de-rencontre-b4121",
-    storageBucket: "site-de-rencontre-b4121.appspot.com",
-    messagingSenderId: "863763706178",
-    appId: "1:863763706178:web:62c3927ca62a30e85e98db",
-    measurementId: "G-47YFCS49EB"
-  };
-  const app = initializeApp(firebaseConfig);
+apiKey: "AIzaSyAI3z9mlINBKqd8CIF18LV-y0FJKrV7jms",
+authDomain: "site-de-rencontre-b4121.firebaseapp.com",
+projectId: "site-de-rencontre-b4121",
+storageBucket: "site-de-rencontre-b4121.appspot.com",
+messagingSenderId: "863763706178",
+appId: "1:863763706178:web:62c3927ca62a30e85e98db",
+measurementId: "G-47YFCS49EB"
+};
 
-  //make authh and firestore references
-  const auth = getAuth(app);
-  const database = getFirestore(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-function register() {
-    const email = document.getElementById('email').value
-    const password = document.getElementById('email').value
-    auth.CreateUserWithEmailAndPassword(email, password).then(function() {
+const submit = document.getElementById('submit');
 
-        var user = auth.CurrentUser;
-
-        var database_ref = database.ref();
-        var user_data = {
-            email : email,
-            password : password
-        }
-        database_ref.child('users/' + user.uid).set(user_data);
-
-        alert("User created")
+submit.addEventListener("click", function(event){
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  console.log(email, password)
+  event.preventDefault();
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed up 
+      const user = userCredential.user;
+      alert("Creating User");
+      // ...
     })
-}
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert("Error");
+      // ..
+    });
+})
